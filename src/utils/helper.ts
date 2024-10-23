f95aed371e6375cc5607befeb5c5b6f165e2e164
anujnema5 <anujnema63@gmail.com>
2024-10-23 17:11:13 +0530

    PROTOTYPE REMOVED

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,10 +1,10 @@
-interface String {
-    firstLetterCapital: () => void;
-}
+// interface String {
+//     firstLetterCapital: () => void;
+// }
 
-String.prototype.firstLetterCapital = function () {
-    return this.charAt(0) + this.slice(1)
-}
+// String.prototype.firstLetterCapital = function () {
+//     return this.charAt(0) + this.slice(1)
+// }
 
 export function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ');
