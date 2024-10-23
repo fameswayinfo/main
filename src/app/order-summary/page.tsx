14e76e3d5cfb1a0c34f47e13ec8be942f043b722
anujnema5 <anujnema63@gmail.com>
2024-10-23 17:06:58 +0530

    prod fix

--- src/app/order-summary/page.tsx
+++ src/app/order-summary/page.tsx
@@ -4,11 +4,9 @@ import { Footer } from '@/components/footer'
 import { GradientBackground } from '@/components/gradient'
 import { Navbar } from '@/components/navbar'
 import OrderSummary from '@/components/order-summary'
-import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
-    const searchParams = useSearchParams();
     return (
         <div className='h-screen flex flex-col relative'>
             <GradientBackground />
